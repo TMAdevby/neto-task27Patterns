dcb836f78df491b56fb959a50a4508a82c84aa87
TMAdevby <mtipun@mail.ru>
2025-11-10 21:49:23 +0300

    pattern2

--- src/main/java/ru/netology/people/Person.java
+++ src/main/java/ru/netology/people/Person.java
@@ -1,11 +1,12 @@
 package ru.netology.people;
+import java.util.Objects;
+import java.util.OptionalInt;
 
 public class Person {
     protected final String name;
     protected final String surname;
-    protected int age;
+    protected Integer age;
     protected String city;
-
 
     public Person(String name, String surname) {
         this.name = name;
@@ -23,5 +24,64 @@ public class Person {
         this.surname = surname;
         this.age = age;
         this.city = city;
+    }
+
+    public boolean hasAge() {
+
+        return age != null;
+
+    }
+    public boolean hasAddress() {
+
+        return city != null && !city.isBlank();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setAddress(String city) {
+        this.city = city;
+    }
+
+    public OptionalInt getAge() {
+
+    return age != null ? OptionalInt.of(age) : OptionalInt.empty();
+
+    }
+
+    public void happyBirthday() {
+        age++;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) && Objects.equals(surname, person.surname) && Objects.equals(age, person.age) && Objects.equals(city, person.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, city);
     }
 }

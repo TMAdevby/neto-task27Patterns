a130387f24124bad0c469aeb519312a909be7b0b
TMAdevby <mtipun@mail.ru>
2025-11-10 18:19:19 +0300

    pattern

---
+++ src/main/java/ru/netology/people/Person.java
@@ -0,0 +1,27 @@
+package ru.netology.people;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String city;
+
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person(String name, String surname, int age, String city) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.city = city;
+    }
+}

1fc80da74611be47165ffccce340a5f782780863
TMAdevby <mtipun@mail.ru>
2025-11-11 21:10:19 +0300

    logger3

--- src/main/java/ru/netology/logger/Main.java
+++ src/main/java/ru/netology/logger/Main.java
@@ -1,5 +1,8 @@
 package ru.netology.logger;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -20,5 +23,19 @@ public class Main {
         Integer m = Integer.parseInt(sc.nextLine());
         logger.log("Введите верхнюю границу для значений:");
 
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+
+        StringBuilder st = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            list.add(random.nextInt(m + 1));
+            st.append(n).append(" ");
+        }
+        logger.log("Создаем и наполняем список \n Вот случайный список: " + st);
+
+
+
+        logger.log("Просим пользователя ввести входные данные для фильтрации\n" +
+                "Введите порог для фильтра: 4" + st);
     }
 }
